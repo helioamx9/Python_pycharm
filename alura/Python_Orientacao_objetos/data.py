e80f5e5b40d50af195abbee54caaeecebfeb75a5
helioamx9 <48929507+helioamx9@users.noreply.github.com>
2021-02-25 14:40:12 -0300

    Delete alura directory

--- alura/Python_Orientacao_objetos/data.py
+++
@@ -1,8 +0,0 @@
-class Data:
-    def __init__(self,dia,mes,ano):
-        self.dia = int(dia)
-        self.mes = int(mes)
-        self.ano = int(ano)
-
-    def formadata(self):
-        print(f"{self.dia}/{self.mes:02}/{self.ano}")
